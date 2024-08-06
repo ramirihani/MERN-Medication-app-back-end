c0660ea2e097618050d289a0f69262cfc066b83f
ramirihani <ramy.rayhani@gmail.com>
2024-08-06 23:15:55 +0100

    multer to add image

---
+++ multerConfig.js
@@ -0,0 +1,32 @@
+// multerConfig.js
+const multer = require("multer");
+const path = require("path");
+
+// Set storage engine
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+  },
+});
+
+// Initialize upload
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png/;
+    const extname = fileTypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+    const mimetype = fileTypes.test(file.mimetype);
+    if (mimetype && extname) {
+      return cb(null, true);
+    } else {
+      cb("Error: Images Only!");
+    }
+  },
+});
+
+module.exports = upload;
